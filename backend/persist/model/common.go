406222fddd5d2808076fa10e0b24ea36beffa77a
lishenglin <shenglin.li@metalpha.finance>
2022-11-15 18:10:30 +0800

    v0.0.4

--- backend/persist/model/common.go
+++ backend/persist/model/common.go
@@ -1,1 +1,16 @@
 package model
+
+type Role string
+
+var (
+	Read  Role = "Read"
+	Write Role = "Write"
+	Admin Role = "Admin"
+)
+
+type DocType string
+
+var (
+	DIR DocType = "DIR"
+	API DocType = "API"
+)
